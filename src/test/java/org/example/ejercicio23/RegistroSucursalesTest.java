937e1c608773392162d610dd8c6ceeb120f4d636
Banda <alebandaber@gmail.com>
2026-04-22 09:01:17 -0300

    b

---
+++ src/test/java/org/example/ejercicio23/RegistroSucursalesTest.java
@@ -0,0 +1,13 @@
+package test.java.org.example.ejercicio23;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+public class RegistroSucursalesTest {
+    @Test
+    public void testEjemplo(){
+        assertFalse(false);
+        assertTrue(true);
+    }
+}
