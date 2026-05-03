c4f780b190d4cd49df471fb03365ad97b8fdb3e5
Sebanev15 <sneves@impulso.edu.uy>
2026-05-03 10:14:01 -0300

    add test structure

---
+++ src/test/java/org/example/Clases/ExampleTest.java
@@ -0,0 +1,13 @@
+package org.example.Clases;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ExampleTest {
+    @Test
+    public void exampleTest() {
+        assertEquals(1,1);
+        assertTrue(true);
+    }
+}
